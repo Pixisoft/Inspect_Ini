4ee7a3b1e9824e3ac7bfc57c581a7047598d7466
JenChieh <jcs090218@gmail.com>
2021-06-12 16:56:49 +0800

    init

---
+++ Assets/Inspect/INI/Editor/IniUtil.cs
@@ -0,0 +1,25 @@
+/**
+ * $File: IniUtil.cs $
+ * $Date: #CREATIONDATE# $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information 
+ *	                 Copyright © #CREATEYEAR# by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class IniUtil 
+    : MonoBehaviour 
+{
+    /* Variables */
+
+    /* Setter & Getter */
+
+    /* Functions */
+
+}
