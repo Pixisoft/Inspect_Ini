16d91d89aff91561d952e5149ca09100e07f1d0a
JenChieh <jcs090218@gmail.com>
2021-06-12 17:36:13 +0800

    Add test

--- Assets/Inspect/INI/Editor/IniEditor.cs
+++ Assets/Inspect/INI/Editor/IniEditor.cs
@@ -1,25 +1,164 @@
+#if UNITY_EDITOR && UNITY_STANDALONE_WIN
 /**
- * $File: IniEditor.cs $
- * $Date: #CREATIONDATE# $
- * $Revision: $
- * $Creator: Jen-Chieh Shen $
- * $Notice: See LICENSE.txt for modification and distribution information 
- *	                 Copyright © #CREATEYEAR# by Shen, Jen-Chieh $
+ * Copyright (c) Pixisoft Corporations. All rights reserved.
+ * 
+ * Licensed under the Source EULA. See COPYING in the asset root for license informtaion.
  */
-using System.Collections;
-using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
 
-/// <summary>
-/// 
-/// </summary>
-public class IniEditor 
-    : MonoBehaviour 
+namespace Inspect.Ini
 {
-    /* Variables */
+    public class IniEditor : Editor
+    {
+        /* Variables */
 
-    /* Setter & Getter */
+        private const int INDENT_SPACE = 15;
+        private string Path => AssetDatabase.GetAssetPath(target);
+        private bool isCompatible => Path.EndsWith(".ini");
+        private bool unableToParse => !IniUtil.IsValidIni(rawText);
 
-    /* Functions */
+        private bool isTextMode;
 
+        private string rawText;
+        private IniFile iniFile;
+
+        /* Setter & Getter */
+
+        /* Functions */
+
+        private void OnEnable()
+        {
+            if (isCompatible)
+                LoadFromIni();
+        }
+
+        private void OnDisable()
+        {
+            if (isCompatible)
+                WriteToIni();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            if (isCompatible)
+                IniInspectorGUI();
+        }
+
+        private void IniInspectorGUI()
+        {
+            GUI.enabled = true;
+
+            const string info = "You edit raw text if the XML editor isn't enough by clicking the button to the right";
+
+            Rect subHeaderRect = EditorGUILayout.GetControlRect(false, EditorGUIUtility.singleLineHeight * 1.8f);
+            Rect helpBoxRect = new Rect(subHeaderRect.x, subHeaderRect.y, subHeaderRect.width - subHeaderRect.width / 6 - 5f, subHeaderRect.height);
+            Rect rawTextModeButtonRect = new Rect(subHeaderRect.x + subHeaderRect.width / 6 * 5, subHeaderRect.y, subHeaderRect.width / 6, subHeaderRect.height);
+            EditorGUI.HelpBox(helpBoxRect, info, MessageType.Info);
+
+            GUIStyle wrappedButton = new GUIStyle("Button");
+            wrappedButton.wordWrap = true;
+            EditorGUI.BeginChangeCheck();
+            isTextMode = GUI.Toggle(rawTextModeButtonRect, isTextMode || unableToParse, "Edit Text", wrappedButton);
+
+            if (EditorGUI.EndChangeCheck() && !unableToParse)
+            {
+                WriteToIni(!isTextMode);
+                GUI.FocusControl("");
+                LoadFromIni();
+            }
+
+            if (isTextMode || unableToParse)
+            {
+                OnTextMode();
+            }
+            else
+            {
+                OnGUIMode();
+            }
+        }
+
+        private void OnGUIMode()
+        {
+            if (iniFile == null)
+                return;
+
+            GUILayout.Space(5);
+
+            //IniUtil.BeginVertical(() => { RecursiveDrawField(xmlDoc.Root); });
+
+            GUILayout.Space(5);
+
+            IniUtil.BeginHorizontal(() =>
+            {
+                const float border = 20.0f;
+                GUILayout.Space(border / 2);
+
+                if (GUILayout.Button("Add New Property", GUILayout.Width(EditorGUIUtility.currentViewWidth - border)))
+                    DrawNewProperty();
+            });
+        }
+
+        private void OnTextMode()
+        {
+            rawText = IniUtil.WithoutSelectAll(() => EditorGUILayout.TextArea(rawText));
+
+            GUIStyle helpBoxRichText = new GUIStyle(EditorStyles.helpBox);
+            Texture errorIcon = EditorGUIUtility.Load("icons/console.erroricon.png") as Texture2D;
+
+            helpBoxRichText.richText = true;
+
+            if (unableToParse)
+            {
+                var content = new GUIContent("Unable to parse text into XML. Make sure there are no mistakes!", errorIcon);
+                GUILayout.Label(content, helpBoxRichText);
+            }
+            else
+            {
+                WriteToIni(true);
+            }
+        }
+
+        private void LoadFromIni()
+        {
+            if (string.IsNullOrWhiteSpace(Path) || !File.Exists(Path))
+                return;
+
+            rawText = File.ReadAllText(Path);
+        }
+
+        private void WriteToIni(bool useRaw = false)
+        {
+            if (!File.Exists(Path))
+                return;
+
+            File.WriteAllText(Path, rawText);
+        }
+
+
+        [MenuItem("Assets/Create/INI File", priority = 82)]
+        public static void CreateNewXmlFile()
+        {
+            string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+            if (path == "")
+                path = "Assets";
+            else if (System.IO.Path.GetExtension(path) != "")
+                path = path.Replace(System.IO.Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+
+            path = System.IO.Path.Combine(path, "New INI File.ini");
+
+            File.WriteAllText(path, "");
+
+            AssetDatabase.Refresh();
+        }
+
+        private void DrawNewProperty()
+        {
+
+        }
+    }
 }
+#endif
--- Assets/Inspect/INI/Editor/IniUtil.cs
+++ Assets/Inspect/INI/Editor/IniUtil.cs
@@ -1,25 +1,69 @@
+#if UNITY_EDITOR && UNITY_STANDALONE_WIN
 /**
- * $File: IniUtil.cs $
- * $Date: #CREATIONDATE# $
- * $Revision: $
- * $Creator: Jen-Chieh Shen $
- * $Notice: See LICENSE.txt for modification and distribution information 
- *	                 Copyright © #CREATEYEAR# by Shen, Jen-Chieh $
+ * Copyright (c) Pixisoft Corporations. All rights reserved.
+ * 
+ * Licensed under the Source EULA. See COPYING in the asset root for license informtaion.
  */
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-/// <summary>
-/// 
-/// </summary>
-public class IniUtil 
-    : MonoBehaviour 
+namespace Inspect.Ini
 {
-    /* Variables */
+    public delegate void EmptyFunction();
 
-    /* Setter & Getter */
+    public static class IniUtil
+    {
+        /* Variables */
 
-    /* Functions */
+        private const string NAME = "Inspect_Ini";
 
+        /* Setter & Getter */
+
+        /* Functions */
+
+        public static bool IsValidIni(string ini)
+        {
+            return true;
+        }
+        public static void BeginHorizontal(EmptyFunction func, bool flexibleSpace = false)
+        {
+            GUILayout.BeginHorizontal();
+            if (flexibleSpace) GUILayout.FlexibleSpace();
+            func.Invoke();
+            GUILayout.EndHorizontal();
+        }
+
+        public static void BeginVertical(EmptyFunction func, string style = "box")
+        {
+            if (style == "")
+                GUILayout.BeginVertical();
+            else
+                GUILayout.BeginVertical("box");
+            func.Invoke();
+            GUILayout.EndVertical();
+        }
+
+        public static Vector2 CalcSize(string text)
+        {
+            return GUI.skin.label.CalcSize(new GUIContent(text));
+        }
+
+        public static T WithoutSelectAll<T>(Func<T> guiCall)
+        {
+            bool preventSelection = (Event.current.type == EventType.MouseDown);
+
+            Color oldCursorColor = GUI.skin.settings.cursorColor;
+
+            if (preventSelection)
+                GUI.skin.settings.cursorColor = new Color(0, 0, 0, 0);
+
+            T value = guiCall();
+
+            if (preventSelection)
+                GUI.skin.settings.cursorColor = oldCursorColor;
+
+            return value;
+        }
+    }
 }
+#endif
